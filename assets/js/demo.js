c745c19dd938df3294c1756c264fe2693770e348
Jacky-lei1 <56170003+Jacky-lei1@users.noreply.github.com>
2019-12-15 20:03:56 +0800

    666

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -17,8 +17,8 @@ $(function () {
     // Initialize the Gallery as video carousel:
     blueimp.Gallery([
         {
-            title: '我的视频',
-            href: 'https://onedrive.gimhoy.com/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3YvcyFBc3YxaE52QnZMN05nbXA5WWFqNFlJVHdWNWRhP2U9bmxRdk5m.mp4',
+            title: '【SAO Util刀剑风格启动器】—《你的名字》樱花粉主题，个性化还原',
+            href: '/src/video/1.mp4',
             type: 'video/mp4',
             poster: '/assets/img/1.jpg'
         },
